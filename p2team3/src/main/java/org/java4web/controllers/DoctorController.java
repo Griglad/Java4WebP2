61ff04d5a3392e693ad12a464c159b0c18b74836
it1439 <it1439@uom.edu.gr>
2018-11-14 12:44:27 +0200

    Uploaded project

---
+++ p2team3/src/main/java/org/java4web/controllers/DoctorController.java
@@ -0,0 +1,10 @@
+package org.java4web.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DoctorController {
+
+
+}
