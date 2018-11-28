d89880216c13e3869b4c72d41e43d6d50f2a55b3
it1439 <it1439@uom.edu.gr>
2018-11-28 16:01:01 +0200

    Deleted SpecialtyDto import from appointment controler

--- p2team3/src/main/java/org/java4web/controllers/AppointmentController.java
+++ p2team3/src/main/java/org/java4web/controllers/AppointmentController.java
@@ -7,7 +7,6 @@ import org.java4web.model.Specialty;
 import org.java4web.repositories.AppointmentRepository;
 import org.java4web.services.AppointmentService;
 import org.java4web.utils.AppointmentDto;
-import org.java4web.utils.SpecialtyDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
